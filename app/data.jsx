b7eaabcd7dc626e3c98f869b0f30e408b7aa8889
bhismapratama <bhismaelki@gmail.com>
2023-06-28 13:32:42 +0700

    Hello

---
+++ app/data.jsx
@@ -0,0 +1,40 @@
+const Data = [
+    {
+        id: 1,
+        class: "tiga",
+        title: "Instagram",
+        bx: "bx bxl-instagram-alt",
+        link: "https://www.instagram.com/bhisma.pratama"
+    },
+    {
+        id: 2,
+        class: "satu",
+        title: "GitHub",
+        bx: "bx bxl-github",
+        link: "https://github.com/bhismapratama/"
+    },
+    {
+        id: 3,
+        class: "lima",
+        title: "LinkedIn",
+        bx: "bx bxl-linkedin-square",
+        link: "https://www.linkedin.com/in/bhisma-pratama/"
+    },
+    {
+        id: 4,
+        class: "dua",
+        title: "Twitter",
+        bx: "bx bxl-twitter",
+        link: "https://twitter.com/bhisma_ep?t=3XrVWiNNQlFxbCvQPJsenQ&s=08"
+    },
+    {
+        id: 5,
+        class: "empat",
+        title: "Tik-Tok",
+        bx: "bx bxl-tiktok",
+        link: "https://tiktok.com/@bhisma.pratama"
+    },
+
+]
+
+export default Data
---
+++ app/page.jsx
@@ -0,0 +1,42 @@
+import './index.css'
+import Image from "next/image"
+import pic from './favicon.ico'
+import Data from "./data"
+
+export default function Home() {
+  return (
+    <section className="w-full h-full items-center flex flex-col">
+      <div className="logo w-full h-full flex justify-center">
+        <Image src={pic} alt="Hello" />
+      </div>
+
+      <div className="name">
+        <h1 className="capitalize text-white p-4">Bhisma <span className="text-red-700">Pratama</span></h1>
+      </div>
+
+      <div className="main w-1/2">
+        <ul className="pt-5 pr-0 pb-16 pl-0 m-0 list-none text-center">
+          {Data.map((item) => {
+            return (
+              <li className="button relative" data-aos="fade-up" data-aos-delay="300" key={item.id}>
+                <a href={item.link} className={`mt-6 mb-6 ml-auto mr-auto 
+                pt-2 pb-2 pl-12 pr-12 max-w-full text-2xl text-center flex items-center
+                justify-center font-semibold ${item.class}`}>
+                <i className={`text-center justify-center items-center mt-0 mr-1.5 mb-0 ml-0 top-0 bottom-0 text-2xl ${item.bx}`}>
+                </i>
+                {item.title}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+
+      <div className="footer pb-8">
+        <h1 className="text-xs text-white cursor-pointer">
+          created with <i className="bx bxs-heart hover:cursor-pointer"></i> by <span className="transform-none hover:cursor-pointer">@bysman00</span> :)
+        </h1>
+      </div>
+    </section>
+  )
+}
